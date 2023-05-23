6a7ade551f538d1bb517b167cf50da9112f577c1
Asma5577 <94056599+Asma5577@users.noreply.github.com>
2023-05-23 12:51:27 +0200

    Update module-info.java

--- GameLab/Core/src/main/java/module-info.java
+++ GameLab/Core/src/main/java/module-info.java
@@ -1,2 +1,11 @@
-module $MODULE_NAME$ {
+module Core {
+    requires Common;
+    requires CommonEnemy;
+    requires CommonBullet;
+    requires CommonAsteroids;
+    requires java.desktop;
+    requires com.badlogic.gdx;
+    uses org.example.asteroids.services.IGamePluginService;
+    uses org.example.asteroids.services.IEntityProcessingService;
+    uses org.example.asteroids.services.IPostEntityProcessingService;
 }
