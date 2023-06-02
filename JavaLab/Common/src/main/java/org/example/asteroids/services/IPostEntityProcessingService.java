5a308d8fe75b4d2af4f776e91288c9c9bcfe4599
Asma5577 <94056599+Asma5577@users.noreply.github.com>
2023-06-02 09:34:32 +0200

    Delete a folder

--- JavaLab/Common/src/main/java/org/example/asteroids/services/IPostEntityProcessingService.java
+++
@@ -1,7 +0,0 @@
-package org.example.asteroids.services;
-import org.example.asteroids.data.GameData;
-import org.example.asteroids.data.World;
-
-public interface IPostEntityProcessingService {
-    void process(GameData gameData, World world);
-}
--- JavaLab/CommonAsteroids/src/main/java/org/example/asteroids/IAsteroidSplitter.java
+++
@@ -1,8 +0,0 @@
-package org.example.asteroids;
-
-import org.example.asteroids.data.Entity;
-import org.example.asteroids.data.World;
-
-public interface IAsteroidSplitter {
-    void createSplitAsteroid(Entity e, World w);
-}
